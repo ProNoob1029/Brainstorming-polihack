3b1d73402215c5261ade2f15f3d2b67345465ce5
lamadalaba <rracovictor@gmail.com>
2024-12-07 16:09:56 +0200

    am lucrat

--- app/src/main/java/com/dragos/brainstorming/MainScreen.kt
+++ app/src/main/java/com/dragos/brainstorming/MainScreen.kt
@@ -1,6 +1,7 @@
 package com.dragos.brainstorming
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,13 +14,18 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -61,7 +67,12 @@ fun MainScreen(
         contentPadding = PaddingValues(16.dp)
     ) {
         item {
-            Text("Good Apps")
+            Text(
+                "Good Apps: ${goodAppList.size}",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier
+                    .padding(vertical = 10.dp)
+            )
         }
 
         items(goodAppList) {
@@ -84,77 +95,105 @@ fun MainScreen(
                     modifier = Modifier
                         .size(60.dp)
                         .fillMaxSize(),
-                    painter = rememberDrawablePainter(MainApplication.packageManager.getApplicationIcon(it.packageName)),
+                    painter = rememberDrawablePainter(
+                        MainApplication.packageManager.getApplicationIcon(
+                            it.packageName
+                        )
+                    ),
                     contentDescription = ""
                 )
             }
         }
 
         item {
-            Text("Bad App List")
+            Text(
+                "Bad App List: ${badAppList.size}",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier
+                    .padding(vertical = 10.dp)
+            )
+
         }
 
         items(badAppList) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 8.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = "${it.appName}: ${it.appTime / 1000 / 60}")
-                Spacer(Modifier.weight(1f))
-                IconButton(
-                    onClick = {
-                        removeClick(it)
-                    }
-                ) {
-                    Icon(Icons.Default.Lock, "lock")
-                }
-                Image(
+            Column {
+                Row(
                     modifier = Modifier
-                        .size(60.dp)
-                        .fillMaxSize(),
-                    painter = rememberDrawablePainter(MainApplication.packageManager.getApplicationIcon(it.packageName)),
-                    contentDescription = ""
-                )
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "${it.appName}: ${it.appTime / 1000 / 60}")
+
+                    Spacer(Modifier.weight(1f))
+                    IconButton(
+                        onClick = {
+                            removeClick(it)
+                        }
+                    ) {
+                        Icon(painterResource(R.drawable.thumb_down_24px), "thumbs down")
+                    }
+                    Image(
+                        modifier = Modifier
+                            .size(60.dp)
+                            .fillMaxSize(),
+                        painter = rememberDrawablePainter(MainApplication.packageManager.getApplicationIcon(it.packageName)),
+                        contentDescription = ""
+                    )
+                }
+                HorizontalDivider()
             }
+
         }
 
         item {
-            Text("All Apps")
+            Text(
+                "All Apps: ${appList.size}",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier
+                    .padding(vertical = 10.dp)
+            )
         }
 
         items(appList) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 8.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = "${it.appName}: ${it.appTime / 1000 / 60}")
-                Spacer(Modifier.weight(1f))
-                IconButton(
-                    onClick = {
-                        goodAppClick(it)
-                    }
-                ) {
-                    Icon(Icons.Default.ThumbUp, "add")
-                }
-                IconButton(
-                    onClick = {
-                        badAppClick(it)
-                    }
-                ) {
-                    Icon(Icons.Default.Lock, "lock")
-                }
-                Image(
+            Column {
+                Row(
                     modifier = Modifier
-                        .size(60.dp)
-                        .fillMaxSize(),
-                    painter = rememberDrawablePainter(MainApplication.packageManager.getApplicationIcon(it.packageName)),
-                    contentDescription = ""
-                )
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "${it.appName}: ${it.appTime / 1000 / 60}")
+                    Spacer(Modifier.weight(1f))
+                    IconButton(
+                        onClick = {
+                            goodAppClick(it)
+                        }
+                    ) {
+                        Icon(Icons.Default.ThumbUp, "add")
+                    }
+                    IconButton(
+                        onClick = {
+                            badAppClick(it)
+                        }
+                    ) {
+                        Icon(painterResource(R.drawable.thumb_down_24px), "thumbs down")
+                    }
+                    Image(
+                        modifier = Modifier
+                            .size(60.dp)
+                            .fillMaxSize(),
+                        painter = rememberDrawablePainter(MainApplication.packageManager.getApplicationIcon(it.packageName)),
+                        contentDescription = ""
+                    )
+
+                }
+                HorizontalDivider()
             }
         }
     }
 }
+
+
+
+
