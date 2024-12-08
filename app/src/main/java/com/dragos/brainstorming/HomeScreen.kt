39a070e2da08cc4438b6180b25cc0415c32942b5
lamadalaba <rracovictor@gmail.com>
2024-12-08 11:41:03 +0200

    fix

--- app/src/main/java/com/dragos/brainstorming/HomeScreen.kt
+++ app/src/main/java/com/dragos/brainstorming/HomeScreen.kt
@@ -1,13 +1,23 @@
 package com.dragos.brainstorming
 
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(
@@ -17,32 +27,63 @@ fun HomeScreen(
     navToIsland: () -> Unit,
 ) {
     Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier
+            .fillMaxSize()
+            .padding(vertical = 100.dp)
+        ,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
     ) {
-        Button(
-            onClick = navToIsland
+        Surface(
+            onClick = navToIsland,
+            modifier = Modifier
+                .fillMaxWidth(),
+
         ) {
-            Text(
-                text = "Island",
-                style = MaterialTheme.typography.headlineLarge
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
             )
+            {
+                Text(
+                    text = "Island",
+                    style = MaterialTheme.typography.headlineLarge
+                )
+            }
         }
-        Button(
-            onClick = navToGoodBadApp
+        Spacer(Modifier.height(10.dp))
+        Surface(
+            onClick = navToGoodBadApp,
+                    modifier = Modifier
+                    .fillMaxWidth(),
         ) {
-            Text(
-                text = "Manage healthy apps",
-                style = MaterialTheme.typography.headlineLarge
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
             )
+            {
+                Text(
+                    text = "Manage healthy apps",
+                    style = MaterialTheme.typography.headlineLarge
+                )
+            }
         }
-        Button(
-            onClick = navToLimits
+        Spacer(Modifier.height(10.dp))
+        Surface(
+            onClick = navToLimits,
+            modifier = Modifier
+                .fillMaxWidth(),
         ) {
-            Text(
-                text = "Manage time limits",
-                style = MaterialTheme.typography.headlineLarge
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
             )
+            {
+                Text(
+                    text = "Manage time limits",
+                    style = MaterialTheme.typography.headlineLarge
+                )
+            }
         }
     }
 }
